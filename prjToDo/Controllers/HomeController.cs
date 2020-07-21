c091ab74f6a8bc4684ec6c5547edcb3dcbfcc191
Wendy Wu <wendy8977@outlook.com>
2020-07-21 18:54:41 +0800

    讀取作業（coding一開始的基本頁面）

--- prjToDo/Controllers/HomeController.cs
+++ prjToDo/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace prjToDo.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            //將tToDo資料表內的紀錄依fDate欄位進行(遞減)排序並(轉成串列)再將結果(指定todos變數)
+            //將tToDo資料表內的紀錄依fDate欄位進行(遞減)排序並(轉成串列)再將結果(指定todos(變數)
             var todos = db.tToDo.OrderByDescending(m => m.fDate).ToList();
             //將(todos待辦事項)的所有紀錄傳到Index.cshtml的View檢視畫面
             return View(todos);

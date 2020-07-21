a398c467df5ebcadfcbda65d13a5e4c6eb44e47d
Wendy Wu <wendy8977@outlook.com>
2020-07-21 18:06:12 +0800

    加入專案檔案。

---
+++ prjToDo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using prjToDo.Models;
+namespace prjToDo.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbToDoEntities db = new dbToDoEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            //將tToDo資料表內的紀錄依fDate欄位進行(遞減)排序並(轉成串列)再將結果(指定todos變數)
+            var todos = db.tToDo.OrderByDescending(m => m.fDate).ToList();
+            //將(todos待辦事項)的所有紀錄傳到Index.cshtml的View檢視畫面
+            return View(todos);
+        }
+    }
+}
---
+++ prjToDo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="prjToDo.MvcApplication" Language="C#" %>

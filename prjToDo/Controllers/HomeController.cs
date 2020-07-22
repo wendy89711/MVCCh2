d93355d7b893391afdd2c7e4471cc5130a09ef53
Wendy Wu <wendy8977@outlook.com>
2020-07-22 15:20:09 +0800

    新增功能

--- prjToDo/Controllers/HomeController.cs
+++ prjToDo/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 
 using prjToDo.Models;
+
 namespace prjToDo.Controllers
 {
     public class HomeController : Controller
@@ -18,6 +19,24 @@ namespace prjToDo.Controllers
             var todos = db.tToDo.OrderByDescending(m => m.fDate).ToList();
             //將(todos待辦事項)的所有紀錄傳到Index.cshtml的View檢視畫面
             return View(todos);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        //在Create.cshtml的檢視頁面按下submit會執行的方法
+        [HttpPost]
+        public ActionResult Create(string fTitle, string fImage, DateTime fDate)
+        {
+            tToDo todo = new tToDo();
+            todo.fTitle = fTitle;
+            todo.fImage = fImage;
+            todo.fDate = fDate;
+            db.tToDo.Add(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }

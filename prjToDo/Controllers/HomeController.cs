7c1a0772b22c39b615c3d3c700404ab4b231cc8b
Wendy Wu <wendy8977@outlook.com>
2020-07-22 18:25:37 +0800

    刪除、修改

--- prjToDo/Controllers/HomeController.cs
+++ prjToDo/Controllers/HomeController.cs
@@ -38,5 +38,35 @@ namespace prjToDo.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        
+        //網址傳入id參數
+        public ActionResult Delete(int id)
+        {
+            var todo = db.tToDo.Where(m => m.fId == id).FirstOrDefault();
+            //按下Delete連結到Home/Delete並傳入URL的id參數，id參數值為該筆待辦事項的編號，接著會刪除該筆資料
+            db.tToDo.Remove(todo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var todo = db.tToDo.Where(m => m.fId == id).FirstOrDefault();
+            return View(todo);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int fId, string fTitle, string fImage, DateTime fDate)
+        {
+            //依fId取得要修改的todo項目
+            var todo = db.tToDo.Where(m => m.fId == fId).FirstOrDefault();
+            todo.fTitle = fTitle;
+            todo.fImage = fImage;
+            todo.fDate = fDate;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
